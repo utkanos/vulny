b4bf12b64affefeeaa3d0ce4a04026a34db52621
utkanos <utkanos@gmail.com>
2015-09-28 15:26:39 -0500

    stock email config test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "admin@vulny.xyz"
+ 
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://vulny.xyz/register/blah'
+    mail(to: @user.email, subject: 'Welcome to the Employee Access Portal!')
+  end
 end
