478e667e0f0774b09c3cdc05ce2e68d79a4a610b
utkanos <utkanos@gmail.com>
2015-09-28 15:51:55 -0500

    configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+ 	:authentication => :plain,
+  	:address => "smtp.mailgun.org",
+  	:port => 587,
+  	:domain => "vulny.xyz",
+  	:user_name => "postmaster@vulny.xyz",
+  	:password => "MYPASSWORD"
+}
 end
