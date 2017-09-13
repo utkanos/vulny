0532a852f191e8536eb2e3ae34171c4fedaea18e
Andy Giannini <andy.giannini@nccgroup.trust>
2017-09-13 14:59:28 -0700

    Adds some legit secure functionality

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def admin
+    if params[:id]
+      id = Base64.urlsafe_decode64(params[:id])
+      @user = User.exists?(id: id) ? User.find(id) : nil
+    else
+      redirect_to "/admin?id=#{Base64.urlsafe_encode64('0')}"
+    end
   end
 
   def console
